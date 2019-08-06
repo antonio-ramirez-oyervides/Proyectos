6cb8b3255ed53a26da3caaf3f9fbfd4f498e95ee
Antonio Ramirez Oyervides <antonio_wings@hotmail.com>
2019-08-06 16:48:12 -0500

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Proyectos.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page2.";
+            ViewBag.Message = "Your application description page3.";
 
             return View();
         }
